e2a79055a9b23d8c034fd48adc125a70a1a66d7a
ideksad <ideksad@hotmail.com>
2014-11-19 12:53:25 +0700

    First Commit

---
+++ Models/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcApplication2.Models
+{
+    public class Song
+    
+   {
+       public int ID { get; set; }
+       public string Title { get; set; }
+       public DateTime ReleaseDate { get; set; }
+       public string Genre { get; set; }
+       public decimal Price { get; set; }
+   }
+
+    public class SongDBContext : DbContext
+    {
+
+
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
